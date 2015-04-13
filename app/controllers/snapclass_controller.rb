b8761e1187b490c6b4c1f0d31da0f3b880a050cb
Kyle Hotchkiss <khotchkiss13@berkeley.edu>
2015-04-13 00:14:55 -0700

    Fixed small bugs
    Fixed a few miscellaneous bugs that were in the code.

--- app/controllers/snapclass_controller.rb
+++ app/controllers/snapclass_controller.rb
@@ -14,20 +14,25 @@ class SnapclassController < ApplicationController
   def create
     @user = current_snapuser
     @class = Snapclass.create(class_params)
-    @user.taught_classes << @class
-    additional_teacher = params[:snapclass][:additional_teachers].split(/ |, |,/)
-    additional_teacher = additional_teacher.select{|email| email!=current_snapuser.email}
-    additional_teacher.each do |teacher|
-      user = Snapuser.find_by_email(teacher)
-      if not user
-        flash[:notice] = "User #{teacher} does not exist"
-        redirect_to new_snapproject_path and return
-      else
-        user.taught_classes << @class
+    if not @class.valid?
+      flash[:alert] = "Missing required fields"
+      redirect_to new_snapclass_path and return
+    else
+      @user.taught_classes << @class
+      additional_teacher = params[:snapclass][:additional_teachers].split(/ |, |,/)
+      additional_teacher = additional_teacher.select{|email| email!=current_snapuser.email}
+      additional_teacher.each do |teacher|
+        user = Snapuser.find_by_email(teacher)
+        if not user
+          flash[:notice] = "User #{teacher} does not exist"
+          redirect_to new_snapclass_path and return
+        else
+          user.taught_classes << @class
+        end
       end
+      flash[:notice] = "Class #{@class.title} successfully created"
+      redirect_to snapclass_path(@class)
     end
-    flash[:notice] = "Class #{@class.title} successfully created"
-    redirect_to snapuser_path(@user)
   end
 
   def edit
@@ -37,20 +42,30 @@ class SnapclassController < ApplicationController
   def update
     @class = Snapclass.find(params[:id])
     @class.update_attributes!(class_params)
-    additional_teacher = params[:snapclass][:additional_teachers].split(/ |, |,/)
-    additional_teacher = additional_teacher.select{|email| email!=current_snapuser.email}
-    additional_teacher.each do |teacher|
-      user = Snapuser.find_by_email(teacher)
-      if not user
-        flash[:notice] = "User #{teacher} does not exist"
-        redirect_to new_snapproject_path and return
-      elsif !@class.teachers.include?(user)
-        user.taught_classes << @class
+    if not @class.valid?
+      flash[:notice] = "Missing required fields"
+      redirect_to edit_snapclass_path(@class) and return
+    else
+      @class.save
+      @class.teachers.each do |teacher|
+        @class.teachers.delete(teacher)
       end
+      current_snapuser.taught_classes << @class
+      additional_teacher = params[:snapclass][:additional_teachers].split(/ |, |,/)
+      additional_teacher = additional_teacher.select{|email| email!=current_snapuser.email}
+      additional_teacher.each do |teacher|
+        user = Snapuser.find_by_email(teacher)
+        if not user
+          flash[:notice] = "User #{teacher} does not exist"
+          redirect_to new_snapproject_path and return
+        elsif !@class.teachers.include?(user)
+          user.taught_classes << @class
+        end
+      end
+      @class.save!
+      flash[:notice] = "#{@class.title} was successfully updated."
+      redirect_to snapclass_path(@class)
     end
-    @class.save!
-    flash[:notice] = "#{@class.title} was successfully updated."
-    redirect_to snapclass_path(@class)
   end
 
   def destroy
--- app/models/snapclass.rb
+++ app/models/snapclass.rb
@@ -2,6 +2,7 @@ class Snapclass < ActiveRecord::Base
   has_many :Snapassignments
   has_and_belongs_to_many :teachers, :class_name => "Snapuser", :join_table => 'class_teachers'
   has_and_belongs_to_many :students, :class_name => "Snapuser", :join_table => 'class_students'
+  validates :title, presence: true
 
   def is_public?
   	return self.is_public
