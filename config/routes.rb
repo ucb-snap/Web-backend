778b2542e26dddf399cefddef47fef73128398b9
Kyle Hotchkiss <khotchkiss13@berkeley.edu>
2015-04-27 03:01:16 -0700

    Cleanup of routes and Readme update
    Cleaned up the routes to be more organized, and updated the Readme to
    reflect the current status of the project.

--- config/routes.rb
+++ config/routes.rb
@@ -1,79 +1,30 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # '/'
   get '/' => 'user#index'
-  get '/user/:id/projects' => 'project#index', as: 'user_projects'
-  get '/user/:id/conversations' => 'message#conversations', as: 'conversations'
-  get '/user/:id/conversations/:conversation_id/messages' => 'message#messages', as: 'messages'
-  #post '/user/:id/conversations/:conversation_id', to: 'user#post_message', as: 'send_message'
 
-  get '/user/:id/conversations/new'=> 'message#new_message', as: 'new_message'
-  post '/user/:id/conversations/:conversation_id/messages/', to: 'message#reply', as: 'reply'
-  post '/user/:id/conversations/new', to: 'message#create_new_message', as: 'create_message'
-
-  delete '/user/:id/conversations/:conversation_id' => 'message#destroy', as: 'delete_message'
-
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  # '/user/'
+  devise_for :users
   resources :user
-  resources :project
-  resources :course
-
+  # '/user/projects/'
+  get '/user/:id/projects' => 'project#index', as: 'user_projects'
+  # '/user/courses/'
   get '/user/:id/courses/teaching' => 'course#taught', :as => :taught_courses
   get '/user/:id/courses/enrolled' => 'course#enrolled', :as => :enrolled_courses
   get '/user/:id/courses/all' => 'course#all_courses', :as => :all_courses
+  # '/user/conversation/'
+  get '/user/:id/conversations' => 'message#conversations', as: 'conversations'
+  get '/user/:id/conversations/:conversation_id/messages' => 'message#messages', as: 'messages'
+  get '/user/:id/conversations/new'=> 'message#new_message', as: 'new_message'
+  post '/user/:id/conversations/:conversation_id/messages/', to: 'message#reply', as: 'reply'
+  post '/user/:id/conversations/new', to: 'message#create_new_message', as: 'create_message'
+  delete '/user/:id/conversations/:conversation_id' => 'message#destroy', as: 'delete_message'
+
+  # '/projects/'
+  resources :project
+  post '/project/:id', to: 'project#comment', as: 'project_comment'
+
+  # '/course/'
+  resources :course
   post '/course/:id/enroll' => 'course#enroll', :as => :course_enroll
   post '/course/:id/unenroll' => 'course#unenroll', :as => :course_unenroll
-  post '/project/:id', to: 'project#comment', as: 'project_comment'
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
